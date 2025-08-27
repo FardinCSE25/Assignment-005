fcffc084408a6e4e1f2ed09a747c8415c8bd8108
Fardin Ahmed <fardin.cse25@gmail.com>
2025-08-27 20:25:08 +0600

    call icon functionality added

--- Script.js
+++ Script.js
@@ -7,3 +7,13 @@ for (const heart of hearts) {
         document.getElementById("heart").innerText = count;
     })
 }
+// Call 
+const Services = document.getElementsByClassName("Call")
+    const serviceName = document.getElementsByClassName("service-name")
+    const serviceNumber = document.getElementsByClassName("service-number")
+for(let i=0; i<Services.length; i++){
+    Services[i].addEventListener("click", function(e){
+        e.preventDefault();
+        alert("Calling" + ' ' + serviceName[i].innerText + ' ' + serviceNumber[i].innerText + '.....')
+    })
+}
