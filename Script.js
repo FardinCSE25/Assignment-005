db8aff1510b56c93d6f96a7a8c92bae6e93ead17
Fardin Ahmed <fardin.cse25@gmail.com>
2025-08-27 23:30:39 +0600

    coins reduction added

--- Script.js
+++ Script.js
@@ -8,12 +8,16 @@ for (const heart of hearts) {
     })
 }
 // Call 
-const Services = document.getElementsByClassName("Call")
+    const Services = document.getElementsByClassName("Call")
     const serviceName = document.getElementsByClassName("service-name")
     const serviceNumber = document.getElementsByClassName("service-number")
+
 for(let i=0; i<Services.length; i++){
     Services[i].addEventListener("click", function(e){
         e.preventDefault();
         alert("Calling" + ' ' + serviceName[i].innerText + ' ' + serviceNumber[i].innerText + '.....')
+        let coins = parseInt(document.getElementById("coins").innerText)
+    let remainingCoins = coins - 20;
+    document.getElementById("coins").innerText = remainingCoins;
     })
 }
